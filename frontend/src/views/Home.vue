7cfa79c25d78a148e1c00e14e1bbfcf3122bd24c
Frank Wei <frank@frankwei.xyz>
2020-09-03 19:18:42 +0800

    style: better home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,14 +3,28 @@
     <pre>
       y3d v2
     </pre>
-    <!-- <h1 class="title">Please select one of them: </h1> -->
-    <form class="ui form">
-      <div class="field">
-        <input v-model="y3dContract" type="text"
-        placeholder="Enter Y3D Token Contract address..."  />
+    <div class="ui center aligned basic segment">
+      <form class="ui form">
+        <div class="field">
+          <input v-model="y3dContract" type="text"
+          placeholder="Enter Y3D Token Contract address..."  />
+        </div>
+        <button type="button" class="ui primary button" @click="goToY3dContract">Go</button>
+      </form>
+      <div class="ui horizontal divider">Or just choose one of them:</div>
+      <div class="list-of">
+        <ul>
+          <li>
+            <router-link to="/y3d/0x2e34f61ffa1605da4ee88a6d10e5d75ba8ce246b">fy3d/yfy3d3d</router-link>
+          </li>
+          <li>
+            <router-link to="/y3d/0x1d8c0ef5639445faca65951423dec250bd0e68fc">SHUIHU/ySHUIHU3d</router-link>
+          </li>
+        </ul>
       </div>
-      <button type="button" class="ui primary button" @click="goToY3dContract">Go</button>
-    </form>
+    </div>
+    <!-- <h1 class="title">Please select one of them: </h1> -->
+    
   </div>
 </template>
 
