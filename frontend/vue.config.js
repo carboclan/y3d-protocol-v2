601a5fb876bd4e6bab3fa4a28cb4d2590726ca8b
Garfield Lee <openpgp@550.moe>
2020-09-07 06:39:00 +0800

    feat: add background

---
+++ frontend/vue.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('@vue/cli-service/types/index').ProjectOptions}
+ */
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      title: 'Y3D Protocol V2'
+    }
+  }
+}
