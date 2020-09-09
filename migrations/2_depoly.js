ebd58aadf21824278ec5f0fdc20649a5c620724a
lychees <lychees67@gmail.com>
2020-09-08 19:39:58 -0700

    Go

--- migrations/2_depoly.js
+++ migrations/2_depoly.js
@@ -1,6 +1,12 @@
 const Y_3dfactory = artifacts.require("y_3dfactory");
 
+const route02 = artifacts.require("UniswapV2Router02");
+
 module.exports = function(deployer) {
   //deployer.deploy(Migrations);
 //  deployer.deploy(Y_3dfactory);
+
+deployer.deploy(route02);
+
+// UniswapV2Router02
 };
