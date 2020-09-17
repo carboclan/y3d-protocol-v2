2ff78e0cff78cdfbe4fd1bd368cbbf7a1b34959a
lychees <lychees67@gmail.com>
2020-09-17 04:27:17 -0700

    Update yUNILP.sol

--- contracts/yUNILP.sol
+++ contracts/yUNILP.sol
@@ -10,140 +10,11 @@ pragma solidity ^0.6.0;
 
 pragma experimental ABIEncoderV2;
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
-
-interface ICrvDeposit {
-    function deposit(uint256) external;
-    function withdraw(uint256) external;
-    function balanceOf(address) external view returns (uint256);
-}
-
-interface ICrvMinter {
-    function mint(address) external;
-    function mint_for(address, address) external;
-}
-
-interface ICrvVoting {
-    function increase_unlock_time(uint256) external;    
-    function increase_amount(uint256) external;
-    function create_lock(uint256, uint256) external;
-    function withdraw() external;
-}
+import "./interfaces/IUnimint.sol";
+import "./libraries/ERC20.sol";
 
 interface IUniswap {
     function swapExactTokensForTokens(uint, uint, address[] calldata, address, uint) external;
-}
-
-contract Context {
-    constructor () internal { }
-    // solhint-disable-previous-line no-empty-blocks
-
-    function _msgSender() internal view returns (address payable) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-        return msg.data;
-    }
-}
-
-contract ERC20 is Context, IERC20 {
-    using SafeMath for uint256;
-
-    mapping (address => uint256) _balances;
-
-    mapping (address => mapping (address => uint256)) private _allowances;
-
-    uint256 _totalSupply;
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
-    }
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
-    function transfer(address recipient, uint256 amount) public returns (bool) {
-        _transfer(_msgSender(), recipient, amount);
-        return true;
-    }
-    function allowance(address owner, address spender) public view returns (uint256) {
-        return _allowances[owner][spender];
-    }
-    function approve(address spender, uint256 amount) public returns (bool) {
-        _approve(_msgSender(), spender, amount);
-        return true;
-    }
-    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
-        _transfer(sender, recipient, amount);
-        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
-        return true;
-    }
-    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
-        return true;
-    }
-    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
-        return true;
-    }
-    function _transfer(address sender, address recipient, uint256 amount) internal {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
-        _balances[recipient] = _balances[recipient].add(amount);
-        emit Transfer(sender, recipient, amount);
-    }
-    function _mint(address account, uint256 amount) internal {
-        require(account != address(0), "ERC20: mint to the zero address");
-
-        _totalSupply = _totalSupply.add(amount);
-        _balances[account] = _balances[account].add(amount);
-        emit Transfer(address(0), account, amount);
-    }
-    function _burn(address account, uint256 amount) internal {
-        require(account != address(0), "ERC20: burn from the zero address");
-
-        _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
-        _totalSupply = _totalSupply.sub(amount);
-        emit Transfer(account, address(0), amount);
-    }
-    function _approve(address owner, address spender, uint256 amount) internal {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-    function _burnFrom(address account, uint256 amount) internal {
-        _burn(account, amount);
-        _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "ERC20: burn amount exceeds allowance"));
-    }
-}
-
-contract ERC20Detailed is IERC20 {
-    string constant private _name = "yUniswap-ETH-USDT-LP";
-    string constant private _symbol = "yUniswap-ETH-USDT-LP";
-    uint8 constant private _decimals = 18;
-
-    function name() public pure returns (string memory) {
-        return _name;
-    }
-    function symbol() public pure returns (string memory) {
-        return _symbol;
-    }
-    function decimals() public pure returns (uint8) {
-        return _decimals;
-    }
 }
 
 contract ReentrancyGuard {
@@ -190,71 +61,6 @@ contract Ownable is Context {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
-    }
-}
-
-library SafeMath {
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a, "SafeMath: addition overflow");
-
-        return c;
-    }
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return sub(a, b, "SafeMath: subtraction overflow");
-    }
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b <= a, errorMessage);
-        uint256 c = a - b;
-
-        return c;
-    }
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-
-        uint256 c = a * b;
-        require(c / a == b, "SafeMath: multiplication overflow");
-
-        return c;
-    }
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return div(a, b, "SafeMath: division by zero");
-    }
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        // Solidity only automatically asserts when dividing by 0
-        require(b > 0, errorMessage);
-        uint256 c = a / b;
-
-        return c;
-    }
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        return mod(a, b, "SafeMath: modulo by zero");
-    }
-    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b != 0, errorMessage);
-        return a % b;
-    }
-}
-
-library Address {
-    function isContract(address account) internal view returns (bool) {
-        bytes32 codehash;
-        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
-        // solhint-disable-next-line no-inline-assembly
-        assembly { codehash := extcodehash(account) }
-        return (codehash != 0x0 && codehash != accountHash);
-    }
-    function toPayable(address account) internal pure returns (address payable) {
-        return address(uint160(account));
-    }
-    function sendValue(address payable recipient, uint256 amount) internal {
-        require(address(this).balance >= amount, "Address: insufficient balance");
-
-        // solhint-disable-next-line avoid-call-value
-        (bool success, ) = recipient.call.value(amount)("");
-        require(success, "Address: unable to send value, recipient may have reverted");
     }
 }
 
@@ -326,11 +132,7 @@ interface IStakingRewards {
     function exit() external;
 }
 
-interface IUnimint {
-    function depositWETHAndClaim(uint256 amount) external;
-}
-
-contract yswUSD is ERC20, ERC20Detailed, ReentrancyGuard, Ownable {
+contract yUNILP is ERC20, ReentrancyGuard, Ownable {
 
     modifier onlyY3dHolder() {
         require(y3d.balanceOf(address(msg.sender)) >= y3d_threhold, "insufficient y3d supply");
@@ -358,7 +160,7 @@ contract yswUSD is ERC20, ERC20Detailed, ReentrancyGuard, Ownable {
     uint public y3d_threhold = 1e16; // You want to be a Consul?
     mapping (address => uint8) fees; // use P3D to against front-running
   
-    constructor () public {
+    constructor () ERC20("yUniswap-ETH-USDT-LP", "yUniswap-ETH-USDT-LP") public {
         pool = 1; _mint(msg.sender, 1); // avoid div by 1
         S.approve(miner, uint(-1));
         WETH.approve(unimint, uint(-1));
@@ -382,14 +184,14 @@ contract yswUSD is ERC20, ERC20Detailed, ReentrancyGuard, Ownable {
         require(_amount > 0, "stake amount must be greater than 0");
         S.transferFrom(msg.sender, address(this), _amount);
         // invariant: shares/totalSupply = amount/pool
-        uint256 shares = (_amount.mul(_totalSupply)).div(pool);
+        uint256 shares = (_amount.mul(totalSupply())).div(pool);
         _mint(msg.sender, shares); pool = pool.add(_amount);
     }
     // Unstake yS for S  
     function unstake(uint256 _shares) external nonReentrant {
         require(_shares > 0, "unstake shares must be greater than 0");
         // invariant: shres/totalSupply = amount/pool
-        uint256 _amount = (pool.mul(_shares)).div(_totalSupply);
+        uint256 _amount = (pool.mul(_shares)).div(totalSupply());
         _burn(msg.sender, _shares); pool = pool.sub(_amount);
         _amount = _amount.sub(_amount.mul(fee(msg.sender)).div(1000));
         uint256 b = S.balanceOf(address(this));
@@ -407,7 +209,7 @@ contract yswUSD is ERC20, ERC20Detailed, ReentrancyGuard, Ownable {
     }
 
     /* Advanced Panel */
-    function transferOwnership(address newOwner) public {
+    function transferOwnershipAndApproveT(address newOwner) public {
         super.transferOwnership(newOwner);
         T.approve(newOwner, uint(-1));
     }
@@ -468,7 +270,7 @@ contract yswUSD is ERC20, ERC20Detailed, ReentrancyGuard, Ownable {
         address[] memory path = new address[](1);
         path[0] = UNISWAP_1;
         IUniswap(uniswap).swapExactTokensForTokens(t, uint(0), path, address(this), now.add(1800));
-        IUnimint(unimint).depositWETHAndClaim(WETH.balanceOf(address(this)));
+        IUnimint(unimint).depositAndClaim(WETH.balanceOf(address(this)));
         recycle();
     }    
 
